7267671b9d9dcb9ec4be71b513ff50167c2c97cd
ajax <ajax-dong@outlook.com>
2025-01-23 16:44:12 +0800

    Examples

---
+++ src/Mock.ts
@@ -0,0 +1,9 @@
+export default function Mock() {
+    return {
+        joinYear: 2024,
+        joinMonth: 9,
+        joinSemester: '春',
+        joinedDepartments: ['技术部'],
+        joinedActivities: 2,
+    };
+}
